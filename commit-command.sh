42a116bc1cfdb1f6bbd5bb03d0591b247d7ed431
Raul Jauregui <raul.jauregui2@icloud.com>
2023-02-27 02:10:41 +0000

    completed Lab

--- commit-command.sh
+++ commit-command.sh
@@ -6,5 +6,5 @@ EOF
 chmod +x .git/hooks/post-commit
 cat <<EOF >.git/hooks/pre-commit
 chmod 600 ~/.ssh/id_rsa && chmod 644 ~/.ssh/id_rsa.pub && ssh-keyscan github.com >> ~/.ssh/known_hosts
-EOF
+lEOF
 chmod +x .git/hooks/pre-commit
